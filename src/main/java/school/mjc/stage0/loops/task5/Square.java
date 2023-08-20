67201765206207c3421d9326fd9083373995a0ac
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 20:02:37 +0300

    Adding: Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,7 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public static void printSquareFrom8s(int sideLength){
+    public void printSquareFrom8s(int sideLength){
         for(int x = 0; x < sideLength; x++){
             for(int y = 0; y < sideLength; y++) {
                 if(x == 0 || x == sideLength-1) {
@@ -14,9 +14,5 @@ public class Square {
             }
             System.out.println();
         }
-    }
-
-    public static void main(String[] args) {
-        printSquareFrom8s(0);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,20 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
+    public static void printTriangle(int cathetusLength) {
+        for(int x = 1; x <= cathetusLength; x++) {
+            for (int y = 1; y <= cathetusLength; y++) {
+                if (y <= x) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printTriangle(3);
     }
 }
