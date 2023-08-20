756c6a66742c2eff8975e380c422d52060dcf000
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 19:53:41 +0300

    Adding: Cross.java

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int middleNumber = sideLength / 2 + 1;
 
+        for(int x = 1; x <= sideLength; x++){
+            for(int y = 1; y <= sideLength; y++) {
+                if(x == middleNumber) {
+                    System.out.print("8");
+                } else if (y == middleNumber) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
     }
 }
