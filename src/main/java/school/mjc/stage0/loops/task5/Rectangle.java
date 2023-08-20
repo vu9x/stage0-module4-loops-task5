ea3f6d3c06c3977a4ad9fab5acce5f9ecf568fdb
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 21:23:02 +0300

    Adding: Rectangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -1,7 +1,22 @@
 package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
-    public void printRectangleFrom8s(int length, int height){
+    public static void printRectangleFrom8s(int length, int height){
+        for(int x = 0; x < height; x++){
+            for(int y = 0; y < length; y++) {
+                if(x == 0 || x == height - 1) {
+                    System.out.print("8");
+                } else if (y == 0 || y == length - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printRectangleFrom8s(0, 0);
     }
 }
