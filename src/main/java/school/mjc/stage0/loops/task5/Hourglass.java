22d349da1a4fd31bcbab74708d84aab8f32760f2
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 21:19:10 +0300

    Adding: Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,38 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+    public static void printHourglassOfGivenSize(int height) {
+        int middle = height % 2 == 0 ? height / 2: height / 2 + 1;
+        int emptyPlace = 0;
 
+        for(int x = 0; x < height; x++) {
+            for (int y = 0; y < height; y++) {
+                if(y < emptyPlace || y + 1 > height - emptyPlace) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("8");
+                }
+            }
+            System.out.println();
+
+
+            if (height % 2 == 0) {
+                if (x < middle - 1) {
+                    emptyPlace++;
+                } else if (x >= middle) {
+                    emptyPlace--;
+                }
+            } else {
+                if (x < middle - 1) {
+                    emptyPlace++;
+                } else if (x >= middle - 1) {
+                    emptyPlace--;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        printHourglassOfGivenSize(0);
     }
 }
