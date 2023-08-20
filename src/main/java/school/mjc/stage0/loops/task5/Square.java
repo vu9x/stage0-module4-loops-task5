9f5a46a77516183faf84ef28b5e5f56059aaaf10
Vu <nguyentuanvu9x@gmail.com>
2023-08-20 19:47:15 +0300

    Adding: Square.java

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -1,7 +1,22 @@
 package school.mjc.stage0.loops.task5;
 
 public class Square {
-    public void printSquareFrom8s(int sideLength){
+    public static void printSquareFrom8s(int sideLength){
+        for(int x = 0; x < sideLength; x++){
+            for(int y = 0; y < sideLength; y++) {
+                if(x == 0 || x == sideLength-1) {
+                    System.out.print("8");
+                } else if (y == 0 || y == sideLength - 1) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printSquareFrom8s(0);
     }
 }
